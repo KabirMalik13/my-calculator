dbf2f0fcc813ae85cd910781c9c55a3916e31ba6
Kabir Malik <kabirmalik@MacBook-Pro.local>
2020-07-09 16:49:24 -0700

    Shows the black block in the middle of the scree

---
+++ src/layout/Calculator/Calculator.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const calculator = () => <main className="calculator"></main>;
+export default calculator;
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
