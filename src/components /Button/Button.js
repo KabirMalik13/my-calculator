c9be161040cbcf0eaa09b87210d13cc17f38385c
Kabir Malik <kabirmalik@macbook-pro.attlocal.net>
2020-07-14 15:47:35 -0700

    the rows are not showing up

--- src/components /Button/Button.js
+++ src/components /Button/Button.js
@@ -1,16 +1,15 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-import Screen from './Screen/Screen';
-import Keypad from './Keypad/Keypad';
-
-const calculator = () => (
-  <main className="calculator">
-    <Screen />
-    <Keypad />
-  </main>
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-export default calculator;
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/layout/Calculator/Keypad/Keypad.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import KeypadRow from './KeypadRow/KeypadRow';
+
+const keypad = () => (
+  <section className="keypad">
+    <keypadRow />
+    <keypadRow />
+    <keypadRow />
+    <keypadRow />
+    <keypadRow />
+  </section>
+);
+
+export default keypad;
