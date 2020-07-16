b2e579d10339ff36af95261d7c1e69f0aba458a1
Kabir Malik <kabirmalik@macbook-pro.attlocal.net>
2020-07-16 09:52:21 -0700

    numbers in the rows are starting to show up

---
+++ src/components/Button/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const button = () => (
+  <button classname="btn">
+    1
+  </button>
+);
+
+export default button;
--- src/layout/Calculator/Keypad/Keypad.js
+++ src/layout/Calculator/Keypad/Keypad.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import KeypadRow from './KeypadRow/KeypadRow';
+import Button from '../../../components/Button/Button';
+
 
 const keypad = () => (
   <section className="keypad">
     <KeypadRow />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
     <KeypadRow />
+
     <KeypadRow />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
     <KeypadRow />
+    
+    <KeypadRow />    
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    <KeypadRow />
+
+    <KeypadRow />    
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    <KeypadRow />
+
+    <KeypadRow />    
+      <Button />
+      <Button />
+      <Button />
+      <Button />
     <KeypadRow />
   </section>
 );
