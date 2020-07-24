1b9d6f4eab28f50cdc014e6e17bb590b79eccd38
Kabir Malik <kabirmalik@MacBook-Pro.local>
2020-07-24 15:11:06 -0700

    Calculator is looking like a calculator now

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const button = () => (
-  <button classname="btn">
-    1
-  </button>
-);
+const button = (props) => {
+  const classes = ['btn'];
+  if (typeof props !== 'undefined' && typeof props.type !== 'undefined')
+    classes.push('btn--' + props.type);
+  return (
+    <button className={classes.join(' ')}>
+      {props.children}
+    </button>
+  );
+}
 
 export default button;
